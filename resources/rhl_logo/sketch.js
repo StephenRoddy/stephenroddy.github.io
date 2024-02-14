26809f8f311ca9b8738fa9988b8a733c1479bd56
Stephen Roddy <roddyst@tcd.ie>
2024-02-14 15:15:41 +0000

    logo

---
+++ resources/rhl_logo/sketch.js
@@ -0,0 +1,87 @@
+//------------------------------------------------------------------------------------
+// Grid code derived from: Author: Jesse L. Chaput                              
+// source https://editor.p5js.org/GDD140-J_Chaput/sketches/xVa-sh40u
+//------------------------------------------------------------------------------------
+
+let img, horizon = [], grid = []; //horizon for mountains & clouds, grid for ground
+let starX = [], starY = [], starR = []; //x and y coordinates plus radius for stars
+
+function preload() {
+  img = loadImage("RHL.png");
+}
+
+function setup() {
+  createCanvas(600, 400);
+  stroke(0, 0, 0,66);
+  strokeWeight(2);
+  
+ 
+  
+  //section dedicated to setting initial vertical grid line positions
+  grid.push(new GridLine(1));
+  grid.push(new GridLine(51));
+  grid.push(new GridLine(101));
+  grid.push(new GridLine(151));
+  grid.push(new GridLine(201));
+  grid.push(new GridLine(251));
+  grid.push(new GridLine(301));
+  grid.push(new GridLine(351));
+  grid.push(new GridLine(401));
+  grid.push(new GridLine(451));
+  grid.push(new GridLine(501));
+  grid.push(new GridLine(551));
+  grid.push(new GridLine(601));
+  
+  
+}
+
+
+function draw() {
+  background(255, 255, 255);
+  
+  
+  image(img, -350, 0);
+  
+  push();
+  fill(255, 255, 255);
+  rect(0, 300, 600, 300);
+  pop();
+  
+  //horizontal grid lines
+  line(0, 316, width, 316);
+  line(0, 336, width, 336);
+  line(0, 361, width, 361);
+  line(0, 391, width, 391);
+  
+  
+  //moving elements
+  for(i = 0; i < grid.length; i++) {
+    grid[i].update();
+  }
+  
+}
+
+
+class GridLine {
+  constructor(x) {
+    this.x = x;
+  }
+  
+  update() {
+    this.x--;
+    if(this.x <= -1) {
+      this.x = 649; //rollover
+    }
+    
+    push();
+    stroke(0, 0, 0,66);
+    strokeWeight(2);
+    line(this.x, 302, map(this.x, -20, 620, -250, 850), 400);
+    pop();
+  }
+}
+function keyPressed() {
+  if (key === 's') {
+    saveGif('mySketch', 5);
+  }
+}
