991f1b3880e2b1f1c84c4c8fd436bc4400d20e76
Stephen Roddy <roddyst@tcd.ie>
2024-02-14 16:22:10 +0000

    Update sketch.js

--- resources/rhl_logo/sketch.js
+++ resources/rhl_logo/sketch.js
@@ -2,20 +2,20 @@
 // Grid code derived from: Author: Jesse L. Chaput                              
 // source https://editor.p5js.org/GDD140-J_Chaput/sketches/xVa-sh40u
 //------------------------------------------------------------------------------------
+// Smaller version
 
 let rhl, grid = []; //horizon
+let horizon;
 
 function preload() {
   rhl = loadImage("RHL.png");
 }
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(500, 333);
   stroke(0, 0, 0,66);
   strokeWeight(2);
-  rhl.resize(rhl.width*.7, rhl.height*.7);
-
- 
+  rhl.resize(rhl.width*.5, rhl.height*.5);
   
   //section dedicated to setting initial vertical grid line positions
   grid.push(new GridLine(1));
@@ -32,6 +32,7 @@ function setup() {
   grid.push(new GridLine(551));
   grid.push(new GridLine(601));
   
+  horizon = height-height/3;
   
 }
 
@@ -40,17 +41,17 @@ function draw() {
   background(255, 255, 255);
   
   
-  image(rhl, -70, 50);
+  image(rhl, 15, 45);
   
   push();
-    line(0, 300, width, 300);
+    line(0, horizon, width, horizon);
   pop();
   
   //horizontal grid lines
-  line(0, 316, width, 316);
-  line(0, 336, width, 336);
-  line(0, 361, width, 361);
-  line(0, 391, width, 391);
+  line(0, horizon+horizon*.1, width, horizon+horizon*.1);
+  line(0, horizon+horizon*.25, width, horizon+horizon*.25);
+  line(0, horizon+horizon*.4, width, horizon+horizon*.4);
+ // line(0, horizon+horizon*.20, width, horizon+horizon*.20);
   
   
   //moving elements
@@ -75,7 +76,8 @@ class GridLine {
     push();
       stroke(0, 0, 0,66);
       strokeWeight(2);
-      line(this.x, 302, map(this.x, -20, 620, -250, 850), 400);
+      line(this.x, horizon, map(this.x, -20, 620, -250, 850), 400);
+    //       line(this.x, horizon, map(this.x, -20, 620, -250, 850), 400); //original
     pop();
   }
 }
